dc3c290736455480b09565704131ba69494c1074
rcoronado <rcoronado@intercorp.com.pe>
2020-09-16 15:15:54 -0500

    change catalogue

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,9 @@
 // import { route as routesPrincipal } from '../views/Principal/route'
 // import { route as routesVertical } from '../views/Vertical/route'
 // import { route as routesCatalog } from '../views/Catalog/route'
-import { route as routesCatalogue } from '../views/NewCatalog/route'
+import { route as routesCatalogue, _base as base_catalogue } from '../views/NewCatalog/route'
 import { route as routesIntelligence } from '../views/MVP/route'
-import { route as routesMVP2, _base as base_catalogue } from '../views/MVP2/route'
+import { route as routesMVP2 } from '../views/MVP2/route'
 
 
 console.log('ROUTES:::', base_catalogue)
@@ -12,9 +12,9 @@ const appRoutes = [
     // routesPages,
     // routesVertical,
     // routesPrincipal,
-    routesMVP2,
-    routesIntelligence,
     routesCatalogue,
+    routesIntelligence,
+    // routesMVP2,
     { redirect: true, path: "", to: base_catalogue.path },
 
 ]
